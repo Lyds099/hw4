46d1be56d6e5e1d9a3e7ddedba847f46356f96d0
blair488 <samantha99fan@gmail.com>
2021-06-09 01:08:57 +0800

    main.cpp

---
+++ part2/main.cpp
@@ -0,0 +1,35 @@
+#include "mbed.h"
+#include "bbcar.h"
+#include "bbcar_rpc.h"
+#include "string.h"
+
+Ticker servo_ticker;
+PwmOut pin5(D5), pin6(D6);
+BufferedSerial pc(USBTX,USBRX); //tx,rx
+BufferedSerial uart(D1,D0); //tx,rx
+
+BBCar car(pin5, pin6, servo_ticker);
+
+void follow_line(char* buf);
+Thread followL_Thread(osPriorityLow);
+char buf[64];
+
+int main() {
+   uart.set_baud(9600);
+   int buf_index = 0;
+   while(1){
+      if(uart.readable()){
+            char recv[1];
+            uart.read(recv, sizeof(recv));
+            pc.write(recv, sizeof(recv));
+            if(recv[0] == '\n') {
+               followL_Thread.start(&follow_line(buf));
+               memset(buf,'0',sizeof(buf));
+               buf_index = 0;
+            }
+            else{
+               buf[buf_index++] = recv[0];
+            }
+      }
+   }
+}
