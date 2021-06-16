4ebb42b2573e79e6fdb8e1ac5399e2645501c045
blair488 <samantha99fan@gmail.com>
2021-06-16 17:05:55 +0800

    update

--- part2/line.py
+++ part2/line.py
@@ -1,35 +1,29 @@
 import pyb, sensor, image, time, math
 
+enable_lens_corr = False
 sensor.reset()
 sensor.set_pixformat(sensor.GRAYSCALE)
 sensor.set_framesize(sensor.QQVGA) # we run out of memory if the resolution is much bigger...
 sensor.skip_frames(time = 2000)
 sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
 sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
-THRESHOLD = (0, 110)
 
 uart = pyb.UART(3,9600,timeout_char=1000)
 uart.init(9600,bits=8,parity = None, stop=1, timeout_char=1000)
 
+find = 0
 while(True):
-   img = sensor.snapshot().binary([THRESHOLD])
-   line = img.get_regression([(255,255)], robust = True)
-   if(line):
-       rho_err = abs(line.rho())-img.width()/2
-       if line.theta()>90:
-           theta_err = line.theta()-180
-       else:
-           theta_err = line.theta()
-       img.draw_line(line.line(), color = 127)
-       if(abs(theta_err)>30):
-          if(theta_err<0):
-             uart.write("/turn/run -50 0.2 \n".encode())
-          elif(theta_err>0):
-             uart.write("/turn/run -50 -0.2 \n".encode())
-       else:
-          uart.write("/goStraight/run -50 \n".encode())
+   img = sensor.snapshot()
+   if enable_lens_corr: img.lens_corr(1.8) # for 2.8mm lens...
+   for line in img.find_line_segments(merge_distance = 200, max_theta_diff = 200):
+       img.draw_line(line.line(), color = (255, 255, 0))
+       uart.write("/goStraight/run -50 \n".encode())
        uart.write(("#x1:%d\r\n" % line.x1()).encode())
        uart.write(("#x2:%d\r\n" % line.x2()).encode())
        uart.write(("#y1:%d\r\n" % line.y1()).encode())
        uart.write(("#y2:%d\r\n" % line.y2()).encode())
+       find = 1
+   if find==0 :
+       uart.write("/stop/run \n".encode())
+
 
