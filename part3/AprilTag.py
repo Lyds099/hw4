f093df0fcae4cd67fd5c4744fa2ee5fc9b91bbf8
blair488 <samantha99fan@gmail.com>
2021-06-16 10:25:30 +0800

    AprilTag.py

--- part3/AprilTag.py
+++ part3/AprilTag.py
@@ -32,6 +32,6 @@ while(True):
           uart.write("l")
       elif (angle <= 355 and angle >= 270):
           uart.write("r")
-      if (found==0):
+   if (found==0):
           uart.write("n")
 
